185ffbd8cba51e91dcd9d96caca3880112c3f946
Haaris, Mohammed <Mohammed.Haaris@in.tesco.com>
2018-03-27 01:31:37 +0530

    corrected async nature

--- main.js
+++ main.js
@@ -1,72 +1,52 @@
-import {Library} from "./lib";
+import { Library } from "./lib";
 import Store from "./shared/store";
 
 const store = new Store();
 const library = new Library(store);
 
 /*eslint-disable no-console */
-export default function main (method, data) {
-  switch(method) {
-    case 'addBook': {
-      library.addBook(data).then((res) => {
-        console.log(res);
-      });
-      break;
-    }
-    case 'addUser': {
-      library.addUser(data).then((res) => {
-        console.log(res);
-      });
-      break;
-    }
-    case 'lendBook': {
-      library.lendBook(data).then((res) => {
-        console.log(res);
-      });
-      break;
-    }
-    case 'returnBook': {
-      library.returnBook(data).then((res) => {
-        console.log(res);
-      });
-      break;
-    }
-    case 'searchBook': {
-      library.searchBook(data).then((res) => {
-        console.log(res);
-      });
-      break;
-    }
-    default:
-      console.log("supply a method and data");
-  }
+export default function main() {
+  library
+    .addBook({
+      id: "1",
+      title: "On the Road",
+      author: "Jack Kerouac"
+    })
+    .then(res => {
+      console.log("added book", res);
+      library
+        .addUser({
+          id: "1",
+          name: "Denis",
+          bookBorrowLimit: 3
+        })
+        .then(res => {
+          console.log("added user ", res);
+          library
+            .lendBook({
+              bookId: "1",
+              userId: "1"
+            })
+            .then(res => {
+              console.log("book lended", res);
+              library
+                .returnBook({
+                  bookId: "1",
+                  userId: "1"
+                })
+                .then(res => {
+                  console.log("book returned", res);
+                });
+              library.searchBook("Road", "Jack").then(res => {
+                console.log("Book searched and found", res);
+              });
+              library.searchUser("Denis").then(res => {
+                console.log("User searched and found", res);
+              });
+            });
+        });
+    });
 }
 /*eslint-enable no-console */
 
-main('addBook', {
-  id: "1",
-  title: "On the Road",
-  author: "Jack Kerouac"
-});
-
-main('addUser', {
-  id: "1",
-  name: "Denis",
-  bookBorrowLimit: 3
-});
-
-main('lendBook', {
-  bookId: "1",
-  userId: "1"
-});
-
-main('searchBook', {
-  author: "Jack",
-  title: "Road"
-});
-
-main('returnBook', {
-  bookId: "1",
-  userId: "1"
-});
-
+main();
